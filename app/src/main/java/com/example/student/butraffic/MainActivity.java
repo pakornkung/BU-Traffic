484793f1f6a0f5e20b5710ad7f5895da292cecc3
Pakorn <phakiiz@gmail.com>
2015-11-14 16:01:29 +0700

    Finish Day1

--- app/src/main/java/com/example/student/butraffic/MainActivity.java
+++ app/src/main/java/com/example/student/butraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.student.butraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,7 +45,9 @@ public class MainActivity extends AppCompatActivity {
                 buttonPlayer.start();
 
                 //Intent to WebView
-
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://youtube.com/"));
+                startActivity(objIntent);
 
 
             }   //Event
